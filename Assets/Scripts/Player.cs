e1482be9ac0ce7918422e39b874eb68b922c79a3
Max-pip <4640034max@gmail.com>
2023-02-19 14:47:41 +0200

    sub check-point

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,13 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Player : MonoBehaviour
 {
+    [Header("Panels")]
+    [SerializeField] private GameObject _losePanel;
+    [SerializeField] private GameObject _winPanel;
+
+    [Header("Player`s setting")]
     [SerializeField] private GameObject _prefab;
     private GameObject _currentPrefab;
     private float _prefabSpeed = 0f;
     private bool _canChangeSize;
+    private bool _isLose = false;
 
     private void Update()
     {
@@ -20,23 +27,33 @@ public class Player : MonoBehaviour
             _prefabSpeed = 0f;
             _canChangeSize = true;
         }
+
+        if (transform.localScale.x < 0.3f)
+        {
+            _isLose = true;
+            _canChangeSize = false;
+            _losePanel.SetActive(true);
+        }
     }
 
     public void PlayerLocalScale()
     {
-        if (_canChangeSize)
+        if (!_isLose)
         {
-            transform.localScale -= new Vector3(0.003f, 0.003f, 0.003f);
-        }
-        if (_currentPrefab == null)
-        {
-            _currentPrefab = (GameObject) Instantiate(_prefab, new Vector3(0, 1, -10), transform.rotation);
-        }
+            if (_canChangeSize)
+            {
+                transform.localScale -= new Vector3(0.003f, 0.003f, 0.003f);
+            }
+            if (_currentPrefab == null)
+            {
+                _currentPrefab = (GameObject)Instantiate(_prefab, new Vector3(0, 1, -10), transform.rotation);
+            }
 
-        if (_currentPrefab != null && _canChangeSize)
-        {
-            _currentPrefab.transform.localScale += new Vector3(0.003f, 0.003f, 0.003f);
-        }
+            if (_currentPrefab != null && _canChangeSize)
+            {
+                _currentPrefab.transform.localScale += new Vector3(0.003f, 0.003f, 0.003f);
+            }
+        }       
     }
 
     public void ButtonUp()
